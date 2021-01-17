141188ddc34bbb4aa6711af36a1750d81c2b3a09
YajePanta <yajepanta@gmail.com>
2021-01-17 00:23:40 -0500

    View by category choosen / fetch wordpress api

--- src/router.js
+++ src/router.js
@@ -5,16 +5,28 @@ const data = {
     proposals: [
         {
             title: "Ejemplo 1",
-            content: "Impulsaremos el apoyo a las empresas que operan Residuos de Aparatos Eléctricos y Electrónicos (RAEE) a nivel nacional con la finalidad de darles soportes para que se amplíen y mantengan dicha actividad.",
+            content: "Impulsaremos el apoyo a las empresas que operan Residuos de Aparatos Eléctricos y Electrónicos (RAEE) a nivel nacional.",
             parties: ["Juntos por el Perú", "Partido Morado"],
-            alert: true
+            alert: true,
         },
-       /*  {
+        {
             title: "Ejemplo 2",
-            content: "Lorem ipsum dolor sit amet consectetur adipiscing elit cras, tellus natoque pellentesque donec montes nunc nec. Himenaeos sed et auctor orci montes pretium cubilia viverra, metus hac ac congue lacus convallis fermentum augue, imperdiet ante ridiculus luctus dignissim faucibus ornare.",
+            content: "Lograr la matriz energética está basada en energías renovables. % de la matriz energética del Perú son renovables.",
             parties: ["APRA", "Partido Morado"],
             alert: false
-        } */
+        },
+        {
+            title: "Ejemplo 2",
+            content: "Lograr la matriz energética está basada en energías renovables. % de la matriz energética del Perú son renovables.",
+            parties: ["APRA", "Partido Morado"],
+            alert: false
+        },
+        {
+            title: "Ejemplo 2",
+            content: "Lograr la matriz energética está basada en energías renovables. % de la matriz energética del Perú son renovables.",
+            parties: ["APRA", "Partido Morado"],
+            alert: false
+        }
     ]
 }
 const main = document.getElementsByTagName('main')[0];
@@ -27,7 +39,7 @@ const changeRoute = (route) => {
             main.appendChild(components.byTopic());
             break;
     
-        default: main.appendChild(components.byTopic(data));
+        default: (data.proposals.map(proposal => main.appendChild(components.byTopic(proposal))));
                 aside.appendChild(components.navBar());
             break;
     }
